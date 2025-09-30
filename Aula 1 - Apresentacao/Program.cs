c44ba843cc51dd2b61a3c084d7e83e65a908bc99
Tiago Castro <tiagocastro044@gmail.com>
2025-09-30 19:21:14 +0100

    CODE-1 - Apresentacao RCII 2025/2026

---
+++ Aula 1 - Apresentacao/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1___Apresentacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World RCII 2025/2026!");
+            Console.ReadKey();
+        }
+    }
+}
