a6c9d8739029c351954ce5f7f4c5af675461533b
Tiago Castro <tiagocastro044@gmail.com>
2025-11-05 16:05:32 +0000

    CODE 6 - Exercícios de Ciclos de Repeticao

---
+++ Aula 3 - Exercicios Ciclos Repeticao/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_3___Exercicios_Ciclos_Repeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Classificar varias notas utilizando o for
+            //Exemplo: nota > 15 ("Bom"), nota >= 9.5 ("suficiente"),
+            // nota < 9.5 ("insuficiente")
+
+            //int qtdDeNotas;
+            //double nota;
+            ////bool AlunoAprovado = false; //true 
+            ////string nome = "Artur Leal"; //texto
+
+            ////C.WL( -> ALTERNATIVA no teste
+            ////obter a quantidade a verificar
+            //Console.WriteLine("Quantidade de notas a verificar: ");
+            //qtdDeNotas = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < qtdDeNotas; i++)
+            //{
+            //    Console.WriteLine("Digita a " + (i + 1) + "º nota: ");
+            //    nota = double.Parse(Console.ReadLine());
+
+            //    if (nota >= 15)
+            //    {
+            //        Console.WriteLine("A nota: " + nota + " têm classificação de bom.");
+            //    }
+            //    else if (nota >= 9.5)
+            //    {
+            //        Console.WriteLine("A nota: " + nota + " têm classificação de suficiente.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("A nota: " + nota + " têm classificação de insuficiente.");
+            //    }
+            //}
+
+            // Para quem já fez
+            // areas ( area > 100 m2 = muito grande), 
+            // areas ( area > 50 m2 = grande), 
+            // areas ( area > 20 m2 = media), 
+            // areas ( area > 5 m2 = pequena), 
+            //int qtdDeAreas;
+            //double area;
+            ////bool AlunoAprovado = false; //true 
+            ////string nome = "Artur Leal"; //texto
+
+            ////C.WL( -> ALTERNATIVA no teste
+            ////obter a quantidade a verificar
+            //Console.WriteLine("Quantidade de areas a verificar: ");
+            //qtdDeAreas = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < qtdDeAreas; i++)
+            //{
+            //    Console.WriteLine("Digita a " + (i + 1) + "º area: ");
+            //    area = double.Parse(Console.ReadLine());
+
+            //    if (area > 100)
+            //    {
+            //        Console.WriteLine("A area: " + area + " têm classificação de muito grande.");
+            //    }
+            //    else if (area > 50)
+            //    {
+            //        Console.WriteLine("A area: " + area + " têm classificação de grande.");
+            //    }
+            //    else if (area > 20)
+            //    {
+            //        Console.WriteLine("A area: " + area + " têm classificação de média.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("A area: " + area + " têm classificação de pequena.");
+            //    }
+            //}
+
+        }
+    }
+}
